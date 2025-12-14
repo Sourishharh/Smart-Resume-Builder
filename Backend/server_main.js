fa55175b75309e6851e2aaacd40faf8002b9df63
Sourishharh <sourishharh@gmail.com>
2025-12-14 18:18:57 +0530

    All Changes

--- Backend/server_main.js
+++ Backend/server_main.js
@@ -6,31 +6,23 @@ import userRouter from "./routes/userRoutes.js";
 import resumeRouter from "./routes/resumeRoutes.js";
 import aiRouter from "./routes/aiRoutes.js";
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-await connectDB();
+// Database connection
+await connectDB()
 
-app.use(express.json());
+app.use(express.json())
+app.use(cors())
 
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:3000",
-      "https://smart-resume-builder-frontend.onrender.com"
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  })
-);
 
-app.get("/", (req, res) => res.send("server is live...."));
+app.get('/', (req, res)=> res.send("server is live...."))
+app.use('/api/users', userRouter)
+app.use('/api/resumes', resumeRouter)
+app.use('/api/ai', aiRouter)
 
-app.use("/api/users", userRouter);
-app.use("/api/resumes", resumeRouter);
-app.use("/api/ai", aiRouter);
-
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
+app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`);
+    
 });
