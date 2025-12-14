ab692ad58863306fdf4cb515cfeb2c3a5be25abe
Sourishharh <sourishharh@gmail.com>
2025-12-14 18:07:57 +0530

    fix: login redirect

--- Frontend/src/pages/Layout.jsx
+++ Frontend/src/pages/Layout.jsx
@@ -1,31 +1,24 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Navigate } from 'react-router-dom';
 import Navber from '../components/Navber';
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import Loader from '../components/Loader';
-import Login from './Login';
-
 
 const Layout = () => {
+  const { user, loading } = useSelector(state => state.auth);
 
-  const {user, loading} = useSelector(state => state.auth)
+  if (loading) return <Loader />;
 
-  if(loading){
-     return <Loader/>
+  if (!user) {
+    return <Navigate to="/login" replace />;
   }
 
   return (
-    <div>
-      {
-        user ? (<div className='min-h-screen bg-gray-50'>
-          <Navber/>
-          <Outlet/>
-          </div>)
-          : <Login/>
-      }
-       
+    <div className="min-h-screen bg-gray-50">
+      <Navber />
+      <Outlet />
     </div>
   );
-}
+};
 
 export default Layout;
